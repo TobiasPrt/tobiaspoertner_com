967afa5088fded6c188bbb03bbcc2fe2f815498b
Tobias Pörtner <t.poertner@outlook.com>
2021-08-14 15:45:43 +0200

    added github action for deployment

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+import { bindNavigationEventHandler } from './navigation/navigationHandler.js';
+import { bindContactFormHandler } from './form/contactFormHandler.js';
+
+
+bindNavigationEventHandler();
+bindContactFormHandler();
